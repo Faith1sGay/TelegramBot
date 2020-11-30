f337e9d390c014f5d958192e32cc59dc32c4e35b
Faith1sGay <daniel.viola001@mymdc.net>
2020-11-30 10:15:52 -0500

    Telegram

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static ITelegramBotClient _bot;
+        static void Main(string[] args)
+        {
+            _bot = new TelegramBotClient("Token for telegram");
+            var me = _bot.GetMeAsync().Result;
+            Console.WriteLine($"Hello World, I am {me.FirstName}");
+            _bot.OnMessage += Bot_OnMessage;
+            _bot.StartReceiving();
+            Console.WriteLine("Press a key to exit!");
+            Console.ReadKey();
+            _bot.StopReceiving();
+        }
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Channel ID: {e.Message.Chat.Id}\nMessage: {e.Message.Text}");
+
+                await _bot.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text: "You said:\n" + e.Message.Text);
+
+            }
+        }
+    }
+}
